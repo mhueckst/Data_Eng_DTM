f91e476d5ea12e43a586f93eacac0a18024d7201
Mahshiiiiid <mahshid75ghasemi@gmail.com>
2023-12-05 18:53:56 -0800

    Edit-initiatedbload.py

--- initiate_db_load.py
+++ initiate_db_load.py
@@ -1,6 +1,15 @@
-import load_to_postgres
-from utilities import get_date_str
+# Importing necessary modules
+import load_to_postgres  # Import the module responsible for loading data into PostgreSQL
+from utilities import get_date_str  # Import 'get_date_str' function from utilities to get today's date in string format
 
-# Get today's date
+# The purpose of this script is to initiate the process of loading data into a PostgreSQL database.
+
+# Constructing the filename for the data file to be loaded
+# The filename includes today's date, ensuring that the data corresponds to the current day
+# 'get_date_str()' is used to append a formatted date string to the filename
 filename = f"/home/dtm-project/consumed_data/{get_date_str()}.csv"
+
+# Execute the main function from the 'load_to_postgres' module
+# This function is tasked with loading the data from the specified CSV file into the PostgreSQL database
 load_to_postgres.main(filename)
+
